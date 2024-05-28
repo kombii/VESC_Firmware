3fd342b9d92cb1b45860f7b42c75b508a5f159ac
kombi <377981654@qq.com>
2024-05-28 16:21:03 +0800

    尝试适配硬件

--- hwconf/kombi/VESC_KS75600/hw_KS75600_core.c
+++ hwconf/kombi/VESC_KS75600/hw_KS75600_core.c
@@ -54,6 +54,7 @@ void hw_init_gpio(void) {
 			PAL_MODE_OUTPUT_PUSHPULL |
 			PAL_STM32_OSPEED_HIGHEST);
 
+	//电机驱动的6个PWM输出，不用修改
 	// GPIOA Configuration: Channel 1 to 3 as alternate function push-pull
 	palSetPadMode(GPIOA, 8, PAL_MODE_ALTERNATE(GPIO_AF_TIM1) |
 			PAL_STM32_OSPEED_HIGHEST |
@@ -86,6 +87,10 @@ void hw_init_gpio(void) {
 			PAL_STM32_OSPEED_HIGHEST);
 	PHASE_FILTER_OFF();
 
+	// Current filter
+	palSetPadMode(GPIOC, 12,
+			PAL_MODE_OUTPUT_PUSHPULL |
+			PAL_STM32_OSPEED_HIGHEST);
 	CURRENT_FILTER_OFF();
 
 	// AUX pin
@@ -94,19 +99,12 @@ void hw_init_gpio(void) {
 			PAL_MODE_OUTPUT_PUSHPULL |
 			PAL_STM32_OSPEED_HIGHEST);
 
-	// Sensor port voltage
-	SENSOR_PORT_3V3();
-	palSetPadMode(SENSOR_VOLTAGE_GPIO, SENSOR_VOLTAGE_PIN,
-			PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-
 	// ADC Pins
 	palSetPadMode(GPIOA, 0, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOA, 1, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOA, 2, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOA, 3, PAL_MODE_INPUT_ANALOG);
-	palSetPadMode(GPIOA, 5, PAL_MODE_INPUT_ANALOG);
-	palSetPadMode(GPIOA, 6, PAL_MODE_INPUT_ANALOG);
-
+	
 	palSetPadMode(GPIOB, 0, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOB, 1, PAL_MODE_INPUT_ANALOG);
 
@@ -115,30 +113,24 @@ void hw_init_gpio(void) {
 	palSetPadMode(GPIOC, 2, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOC, 3, PAL_MODE_INPUT_ANALOG);
 	palSetPadMode(GPIOC, 4, PAL_MODE_INPUT_ANALOG);
-
-	// DAC as voltage reference for shunt amps
-	palSetPadMode(GPIOA, 4, PAL_MODE_INPUT_ANALOG);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
-	DAC->CR |= DAC_CR_EN1;
-	DAC->DHR12R1 = 2047;
 }
 
 void hw_setup_adc_channels(void) {
 	// ADC1 regular channels
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_15Cycles);			// 0
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 2, ADC_SampleTime_15Cycles);			// 3
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 3, ADC_SampleTime_15Cycles);			// 6
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 3, ADC_SampleTime_15Cycles);			// 6
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_14, 4, ADC_SampleTime_15Cycles);			// 9
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_Vrefint, 5, ADC_SampleTime_15Cycles);	// 12
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 6, ADC_SampleTime_15Cycles);			// 15
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_Vrefint, 6, ADC_SampleTime_15Cycles);	// 15
 
 	// ADC2 regular channels
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 1, ADC_SampleTime_15Cycles);			// 1
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 2, ADC_SampleTime_15Cycles);			// 4
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_6, 3, ADC_SampleTime_15Cycles);			// 7
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_15, 4, ADC_SampleTime_15Cycles);			// 10
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_9, 3, ADC_SampleTime_15Cycles);			// 7
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_Vrefint, 4, ADC_SampleTime_15Cycles);	// 10
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_0, 5, ADC_SampleTime_15Cycles);			// 13
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_9, 6, ADC_SampleTime_15Cycles);			// 16
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_Vrefint, 6, ADC_SampleTime_15Cycles);	// 16
 
 	// ADC3 regular channels
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_12, 1, ADC_SampleTime_15Cycles);			// 2
@@ -146,7 +138,7 @@ void hw_setup_adc_channels(void) {
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_3, 3, ADC_SampleTime_15Cycles);			// 8
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 4, ADC_SampleTime_15Cycles);			// 11
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_1, 5, ADC_SampleTime_15Cycles);			// 14
-	ADC_RegularChannelConfig(ADC3, ADC_Channel_2, 6, ADC_SampleTime_15Cycles);			// 17
+	ADC_RegularChannelConfig(ADC3, ADC_Channel_Vrefint, 6, ADC_SampleTime_15Cycles);	// 17
 
 	// Injected channels
 	ADC_InjectedChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_15Cycles);
@@ -161,191 +153,19 @@ void hw_setup_adc_channels(void) {
 
 	chThdCreateStatic(mux_thread_wa, sizeof(mux_thread_wa), NORMALPRIO, mux_thread, NULL);
 }
-
-void hw_start_i2c(void) {
-	i2cAcquireBus(&HW_I2C_DEV);
-
-	if (!i2c_running) {
-		palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
-				PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-		palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
-				PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-
-		i2cStart(&HW_I2C_DEV, &i2cfg);
-		i2c_running = true;
-	}
-
-	i2cReleaseBus(&HW_I2C_DEV);
-}
-
-void hw_stop_i2c(void) {
-	i2cAcquireBus(&HW_I2C_DEV);
-
-	if (i2c_running) {
-		palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN, PAL_MODE_INPUT);
-		palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN, PAL_MODE_INPUT);
-
-		i2cStop(&HW_I2C_DEV);
-		i2c_running = false;
-
-	}
-
-	i2cReleaseBus(&HW_I2C_DEV);
-}
-
-/**
- * Try to restore the i2c bus
- */
-void hw_try_restore_i2c(void) {
-	if (i2c_running) {
-		i2cAcquireBus(&HW_I2C_DEV);
-
-		palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-
-		palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-
-		palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
-		palSetPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
-
-		chThdSleep(1);
-
-		for(int i = 0;i < 16;i++) {
-			palClearPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
-			chThdSleep(1);
-			palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
-			chThdSleep(1);
-		}
-
-		// Generate start then stop condition
-		palClearPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
-		chThdSleep(1);
-		palClearPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
-		chThdSleep(1);
-		palSetPad(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN);
-		chThdSleep(1);
-		palSetPad(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN);
-
-		palSetPadMode(HW_I2C_SCL_PORT, HW_I2C_SCL_PIN,
-				PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-
-		palSetPadMode(HW_I2C_SDA_PORT, HW_I2C_SDA_PIN,
-				PAL_MODE_ALTERNATE(HW_I2C_GPIO_AF) |
-				PAL_STM32_OTYPE_OPENDRAIN |
-				PAL_STM32_OSPEED_MID1 |
-				PAL_STM32_PUDR_PULLUP);
-
-		HW_I2C_DEV.state = I2C_STOP;
-		i2cStart(&HW_I2C_DEV, &i2cfg);
-
-		i2cReleaseBus(&HW_I2C_DEV);
-	}
-}
-
-static THD_FUNCTION(mux_thread, arg) {
-	(void)arg;
-
-	chRegSetThreadName("adc_mux");
-
-	palSetPadMode(ADC_SW_EN_PORT, ADC_SW_EN_PIN,
-			PAL_MODE_OUTPUT_PUSHPULL |
-			PAL_STM32_OSPEED_HIGHEST);
-	palSetPadMode(ADC_SW_1_PORT, ADC_SW_1_PIN ,
-			PAL_MODE_OUTPUT_PUSHPULL |
-			PAL_STM32_OSPEED_HIGHEST);
-	palSetPadMode(ADC_SW_2_PORT, ADC_SW_2_PIN,
-			PAL_MODE_OUTPUT_PUSHPULL |
-			PAL_STM32_OSPEED_HIGHEST);
-	palSetPadMode(ADC_SW_3_PORT, ADC_SW_3_PIN ,
-			PAL_MODE_OUTPUT_PUSHPULL |
-			PAL_STM32_OSPEED_HIGHEST);
-
-#define T_SAMP_US		400
-
-	for (;;) {
-		ADCMUX_MOT_TEMP();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_TEMP_MOTOR] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_VIN();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_VIN_SENS] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_MOS_TEMP1();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_TEMP_MOS] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_MOS_TEMP2();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_TEMP_MOS_2] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_NC();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_NC] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_EXT8();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_EXT8] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_EXT7();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_EXT7] = ADC_Value[ADC_IND_ADC_MUX];
-
-		ADCMUX_MOS_TEMP3();
-		chThdSleepMicroseconds(T_SAMP_US);
-		ADC_Value[ADC_IND_TEMP_MOS_3] = ADC_Value[ADC_IND_ADC_MUX];
-	}
-}
-
-bool hw_sample_shutdown_button(void) {
-	chMtxLock(&shutdown_mutex);
-
-	bt_diff = 0.0;
-
-	for (int i = 0;i < 3;i++) {
-		palSetPadMode(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN, PAL_MODE_INPUT_ANALOG);
-		chThdSleep(5);
-		float val1 = ADC_VOLTS(ADC_IND_SHUTDOWN);
-		chThdSleepMilliseconds(1);
-		float val2 = ADC_VOLTS(ADC_IND_SHUTDOWN);
-		palSetPadMode(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN, PAL_MODE_OUTPUT_PUSHPULL);
-		chThdSleepMilliseconds(1);
-
-		bt_diff += (val1 - val2);
-	}
-
-	chMtxUnlock(&shutdown_mutex);
-
-	return (bt_diff > 0.12);
-}
-
-float hw100_400_get_temp(void) {
+float hwks75600_get_temp(void) {
 	float t1 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
-	float t2 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_2]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
-	float t3 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_3]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
-	float res = 0.0;
+	// float t2 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_2]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
+	// float t3 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_3]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
+	// float res = 0.0;
 
-	if (t1 > t2 && t1 > t3) {
-		res = t1;
-	} else if (t2 > t1 && t2 > t3) {
-		res = t2;
-	} else {
-		res = t3;
-	}
+	// if (t1 > t2 && t1 > t3) {
+	// 	res = t1;
+	// } else if (t2 > t1 && t2 > t3) {
+	// 	res = t2;
+	// } else {
+	// 	res = t3;
+	// }
 
-	return res;
+	return t1;
 }
--- hwconf/kombi/VESC_KS75600/hw_KS75600_core.h
+++ hwconf/kombi/VESC_KS75600/hw_KS75600_core.h
@@ -29,80 +29,59 @@
 #endif
 
 // HW properties
-#define HW_HAS_3_SHUNTS
-#define HW_HAS_PHASE_FILTERS
+#define HW_HAS_3_SHUNTS //三分流电阻
+#define HW_HAS_PHASE_FILTERS //相线电压滤波器（模拟开关控制的一个滤波电阻）
+#define HW_HAS_PHASE_SHUNTS//相线分流电阻采样模式//TODO测试这个，因为str500没有这个
 
 // Macros
-#define LED_GREEN_GPIO			GPIOB
-#define LED_GREEN_PIN			5
-#define LED_RED_GPIO			GPIOB
-#define LED_RED_PIN				7
+#define LED_GREEN_GPIO			GPIOC
+#define LED_GREEN_PIN			  5
+#define LED_RED_GPIO			  GPIOA
+#define LED_RED_PIN				  15
 
 #define LED_GREEN_ON()			palSetPad(LED_GREEN_GPIO, LED_GREEN_PIN)
 #define LED_GREEN_OFF()			palClearPad(LED_GREEN_GPIO, LED_GREEN_PIN)
-#define LED_RED_ON()			palSetPad(LED_RED_GPIO, LED_RED_PIN)
-#define LED_RED_OFF()			palClearPad(LED_RED_GPIO, LED_RED_PIN)
+#define LED_RED_ON()			  palSetPad(LED_RED_GPIO, LED_RED_PIN)
+#define LED_RED_OFF()			  palClearPad(LED_RED_GPIO, LED_RED_PIN)
 
-#define PHASE_FILTER_GPIO		GPIOC
-#define PHASE_FILTER_PIN		9
-#define PHASE_FILTER_ON()		palSetPad(PHASE_FILTER_GPIO, PHASE_FILTER_PIN)
+#define PHASE_FILTER_GPIO		  GPIOC
+#define PHASE_FILTER_PIN		  9
+#define PHASE_FILTER_ON()		  palSetPad(PHASE_FILTER_GPIO, PHASE_FILTER_PIN)
 #define PHASE_FILTER_OFF()		palClearPad(PHASE_FILTER_GPIO, PHASE_FILTER_PIN)
 
-// ADC Mux
-#define ADC_SW_EN_PORT			GPIOB
-#define ADC_SW_EN_PIN			2
-#define ADC_SW_1_PORT			GPIOC
-#define ADC_SW_1_PIN			14
-#define ADC_SW_2_PORT			GPIOC
-#define ADC_SW_2_PIN			15
-#define ADC_SW_3_PORT			GPIOD
-#define ADC_SW_3_PIN			2
+//#define AUX_GPIO				GPIOA
+//#define AUX_PIN					15
+//#define AUX_ON()				palSetPad(AUX_GPIO, AUX_PIN)
+//#define AUX_OFF()				palClearPad(AUX_GPIO, AUX_PIN)
 
-#define AD_DIS()				palClearPad(ADC_SW_EN_PORT, ADC_SW_EN_PIN)
-#define AD1_L()					palClearPad(ADC_SW_1_PORT, ADC_SW_1_PIN)
-#define AD1_H()					palSetPad(ADC_SW_1_PORT, ADC_SW_1_PIN)
-#define AD2_L()					palClearPad(ADC_SW_2_PORT, ADC_SW_2_PIN)
-#define AD2_H()					palSetPad(ADC_SW_2_PORT, ADC_SW_2_PIN)
-#define AD3_L()					palClearPad(ADC_SW_3_PORT, ADC_SW_3_PIN)
-#define AD3_H()					palSetPad(ADC_SW_3_PORT, ADC_SW_3_PIN)
-#define AD_EN()					palSetPad(ADC_SW_EN_PORT, ADC_SW_EN_PIN)
-
-#define ADCMUX_MOT_TEMP()		AD_DIS();	AD3_L();	AD2_L();	AD1_L();	AD_EN();
-#define ADCMUX_VIN()			AD_DIS();	AD3_L();	AD2_L();	AD1_H();	AD_EN();
-#define ADCMUX_MOS_TEMP1()		AD_DIS();	AD3_L();	AD2_H();	AD1_L();	AD_EN();
-#define ADCMUX_MOS_TEMP2()		AD_DIS();	AD3_L();	AD2_H();	AD1_H();	AD_EN();
-#define ADCMUX_NC()				AD_DIS();	AD3_H();	AD2_L();	AD1_L();	AD_EN();
-#define ADCMUX_EXT8()			AD_DIS();	AD3_H();	AD2_L();	AD1_H();	AD_EN();
-#define ADCMUX_EXT7()			AD_DIS();	AD3_H();	AD2_H();	AD1_L();	AD_EN();
-#define ADCMUX_MOS_TEMP3()		AD_DIS();	AD3_H();	AD2_H();	AD1_H();	AD_EN();
-
-#define AUX_GPIO				GPIOA
-#define AUX_PIN					15
-#define AUX_ON()				palSetPad(AUX_GPIO, AUX_PIN)
-#define AUX_OFF()				palClearPad(AUX_GPIO, AUX_PIN)
-
-// Shutdown pin
-#define HW_SHUTDOWN_GPIO		GPIOC
-#define HW_SHUTDOWN_PIN			5
-#define HW_SHUTDOWN_HOLD_ON()	palSetPad(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN)
-#define HW_SHUTDOWN_HOLD_OFF()	palClearPad(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN)
-#define HW_SAMPLE_SHUTDOWN()	hw_sample_shutdown_button()
-
-// Hold shutdown pin early to wake up on short pulses
-#define HW_EARLY_INIT()			palSetPadMode(HW_SHUTDOWN_GPIO, HW_SHUTDOWN_PIN, PAL_MODE_OUTPUT_PUSHPULL); \
-								HW_SHUTDOWN_HOLD_ON();
-
-// Sensor port voltage control
-#define SENSOR_VOLTAGE_GPIO		GPIOC
-#define SENSOR_VOLTAGE_PIN		12
-#define SENSOR_PORT_5V()		palSetPad(SENSOR_VOLTAGE_GPIO, SENSOR_VOLTAGE_PIN)
-#define SENSOR_PORT_3V3()		palClearPad(SENSOR_VOLTAGE_GPIO, SENSOR_VOLTAGE_PIN)
+#define CURRENT_FILTER_ON()		palSetPad(GPIOC, 12)//TODO测试这个，因为str500没有这个
+#define CURRENT_FILTER_OFF()	palClearPad(GPIOC, 12)//TODO测试这个，因为str500没有这个
 
 /*
  * ADC Vector
+ *
+ * 0  (1):	IN0		SENS1
+ * 1  (2):	IN1		SENS2
+ * 2  (3):	IN2		SENS3
+ * 3  (1):	IN10	CURR1
+ * 4  (2):	IN11	CURR2
+ * 5  (3):	IN12	CURR3
+ * 6  (1):	IN5		ADC_EXT1
+ * 7  (2):	IN6		ADC_EXT2
+ * 8  (3):	IN3		TEMP_MOS
+ * 9  (1):	IN14	TEMP_MOTOR
+ * 10 (2):	IN15	ADC_EXT3
+ * 11 (3):	IN13	AN_IN
+ * 12 (1):	Vrefint
+ * 13 (2):	IN0		SENS1
+ * 14 (3):	IN1		SENS2
+ * 15 (1):  IN8		TEMP_MOS_2
+ * 16 (2):  IN9		TEMP_MOS_3
+ * 17 (3):  IN3		SENS3
  */
+
 #define HW_ADC_CHANNELS			18
-#define HW_ADC_CHANNELS_EXTRA	8
+//#define HW_ADC_CHANNELS_EXTRA	8
 #define HW_ADC_INJ_CHANNELS		3
 #define HW_ADC_NBR_CONV			6
 
@@ -113,25 +92,15 @@
 #define ADC_IND_CURR1			0
 #define ADC_IND_CURR2			1
 #define ADC_IND_CURR3			2
-#define ADC_IND_EXT5			11
+#define ADC_IND_VIN_SENS		11
 #define ADC_IND_EXT				6
 #define ADC_IND_EXT2			7
-#define ADC_IND_SHUTDOWN		10
-#define ADC_IND_EXT3			8
-#define ADC_IND_EXT6			9
+#define ADC_IND_EXT3			10
+#define ADC_IND_TEMP_MOS		8
+#define ADC_IND_TEMP_MOS_2		15
+#define ADC_IND_TEMP_MOS_3		16
+#define ADC_IND_TEMP_MOTOR		9
 #define ADC_IND_VREFINT			12
-#define ADC_IND_ADC_MUX			15
-#define ADC_IND_EXT4			16
-
-#define ADC_IND_TEMP_MOTOR		18
-#define ADC_IND_VIN_SENS		19
-#define ADC_IND_TEMP_MOS		20
-#define ADC_IND_TEMP_MOS_2		21
-#define ADC_IND_NC				22
-#define ADC_IND_EXT8			23
-#define ADC_IND_EXT7			24
-#define ADC_IND_TEMP_MOS_3		25
-
 // ADC macros and settings
 
 // Component parameters (can be overridden)
@@ -139,17 +108,18 @@
 #define V_REG					3.3
 #endif
 #ifndef VIN_R1
-#define VIN_R1					150000.0
+#define VIN_R1					56000.0
 #endif
 #ifndef VIN_R2
-#define VIN_R2					4700.0
+#define VIN_R2					2200.0
 #endif
 #ifndef CURRENT_AMP_GAIN
-#define CURRENT_AMP_GAIN		50.0
+#define CURRENT_AMP_GAIN		0.6622516556291391//放大器前面有一个5.1k和10k分压，放大器只做跟随
 #endif
 #ifndef CURRENT_SHUNT_RES
 #ifdef HWKS75600
-#define CURRENT_SHUNT_RES		(0.0002 / 4.0)
+#define CURRENT_SHUNT_RES		(0.004)
+//500A-2000mv的转换关系，在5V的传感器上转换比为0.004v/a（相当于0.004欧电阻）
 #else
 #define CURRENT_SHUNT_RES		(0.0001 / 4.0)
 #endif
@@ -159,6 +129,7 @@
 #define GET_INPUT_VOLTAGE()		((V_REG / 4095.0) * (float)ADC_Value[ADC_IND_VIN_SENS] * ((VIN_R1 + VIN_R2) / VIN_R2))
 
 // NTC Termistors
+//TODO NTC参数后面要调整，包括电机和mos的
 #define NTC_RES(adc_val)		((4095.0 * 10000.0) / adc_val - 10000.0)
 #define NTC_TEMP(adc_ind)		hw100_400_get_temp()
 
@@ -173,13 +144,13 @@
 #define ADC_VOLTS(ch)			((float)ADC_Value[ch] / 4096.0 * V_REG)
 
 // COMM-port ADC GPIOs
-#define HW_ADC_EXT_GPIO			GPIOA
-#define HW_ADC_EXT_PIN			5
-#define HW_ADC_EXT2_GPIO		GPIOA
-#define HW_ADC_EXT2_PIN			6
+#define HW_ADC_EXT_GPIO			GPIOB
+#define HW_ADC_EXT_PIN			0
+#define HW_ADC_EXT2_GPIO		GPIOB
+#define HW_ADC_EXT2_PIN			1
 
 // UART Peripheral
-#define HW_UART_DEV				SD3
+#define HW_UART_DEV				  SD3
 #define HW_UART_GPIO_AF			GPIO_AF_USART3
 #define HW_UART_TX_PORT			GPIOB
 #define HW_UART_TX_PIN			10
@@ -188,7 +159,7 @@
 
 // Permanent UART Peripheral (SWD/ESP)
 #define HW_UART_P_BAUD			115200
-#define HW_UART_P_DEV			SD4
+#define HW_UART_P_DEV			  SD4
 #define HW_UART_P_GPIO_AF		GPIO_AF_UART4
 #define HW_UART_P_TX_PORT		GPIOC
 #define HW_UART_P_TX_PIN		10
@@ -197,21 +168,13 @@
 
 // ICU Peripheral for servo decoding
 #define HW_USE_SERVO_TIM4
-#define HW_ICU_TIMER			TIM4
+#define HW_ICU_TIMER			  TIM4
 #define HW_ICU_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
-#define HW_ICU_DEV				ICUD4
+#define HW_ICU_DEV				  ICUD4
 #define HW_ICU_CHANNEL			ICU_CHANNEL_1
 #define HW_ICU_GPIO_AF			GPIO_AF_TIM4
-#define HW_ICU_GPIO				GPIOB
-#define HW_ICU_PIN				6
-
-// I2C Peripheral
-#define HW_I2C_DEV				I2CD2
-#define HW_I2C_GPIO_AF			GPIO_AF_I2C2
-#define HW_I2C_SCL_PORT			GPIOB
-#define HW_I2C_SCL_PIN			10
-#define HW_I2C_SDA_PORT			GPIOB
-#define HW_I2C_SDA_PIN			11
+#define HW_ICU_GPIO				  GPIOB
+#define HW_ICU_PIN				  6
 
 // Hall/encoder pins
 #define HW_HALL_ENC_GPIO1		GPIOC
@@ -220,8 +183,8 @@
 #define HW_HALL_ENC_PIN2		7
 #define HW_HALL_ENC_GPIO3		GPIOC
 #define HW_HALL_ENC_PIN3		8
-#define HW_ENC_TIM				TIM3
-#define HW_ENC_TIM_AF			GPIO_AF_TIM3
+#define HW_ENC_TIM				  TIM3
+#define HW_ENC_TIM_AF			  GPIO_AF_TIM3
 #define HW_ENC_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE)
 #define HW_ENC_EXTI_PORTSRC		EXTI_PortSourceGPIOC
 #define HW_ENC_EXTI_PINSRC		EXTI_PinSource8
@@ -232,10 +195,10 @@
 #define HW_ENC_TIM_ISR_VEC		TIM3_IRQHandler
 
 // SPI pins
-#define HW_SPI_DEV				SPID1
+#define HW_SPI_DEV				  SPID1
 #define HW_SPI_GPIO_AF			GPIO_AF_SPI1
-#define HW_SPI_PORT_NSS			GPIOB
-#define HW_SPI_PIN_NSS			11
+#define HW_SPI_PORT_NSS			GPIOA
+#define HW_SPI_PIN_NSS			4
 #define HW_SPI_PORT_SCK			GPIOA
 #define HW_SPI_PIN_SCK			5
 #define HW_SPI_PORT_MOSI		GPIOA
@@ -244,22 +207,22 @@
 #define HW_SPI_PIN_MISO			6
 
 // IMU
-//#define LSM6DS3_NSS_GPIO		GPIOC
-//#define LSM6DS3_NSS_PIN			13
-//#define LSM6DS3_SCK_GPIO		GPIOB
-//#define LSM6DS3_SCK_PIN			12
-//#define LSM6DS3_MOSI_GPIO		GPIOB
-//#define LSM6DS3_MOSI_PIN		3
-//#define LSM6DS3_MISO_GPIO		GPIOB
-//#define LSM6DS3_MISO_PIN		4
-#define BMI160_SPI_PORT_NSS		GPIOC
-#define BMI160_SPI_PIN_NSS		13
-#define BMI160_SPI_PORT_SCK		GPIOB
-#define BMI160_SPI_PIN_SCK		12
-#define BMI160_SPI_PORT_MOSI	GPIOB
-#define BMI160_SPI_PIN_MOSI		3
-#define BMI160_SPI_PORT_MISO	GPIOB
-#define BMI160_SPI_PIN_MISO		4
+// #define LSM6DS3_NSS_GPIO		GPIOC
+// #define LSM6DS3_NSS_PIN			13
+// #define LSM6DS3_SCK_GPIO		GPIOB
+// #define LSM6DS3_SCK_PIN			12
+// #define LSM6DS3_MOSI_GPIO		GPIOB
+// #define LSM6DS3_MOSI_PIN		3
+// #define LSM6DS3_MISO_GPIO		GPIOB
+// #define LSM6DS3_MISO_PIN		4
+// #define BMI160_SPI_PORT_NSS		GPIOC
+// #define BMI160_SPI_PIN_NSS		13
+// #define BMI160_SPI_PORT_SCK		GPIOB
+// #define BMI160_SPI_PIN_SCK		12
+// #define BMI160_SPI_PORT_MOSI	GPIOB
+// #define BMI160_SPI_PIN_MOSI		3
+// #define BMI160_SPI_PORT_MISO	GPIOB
+// #define BMI160_SPI_PIN_MISO		4
 
 // Measurement macros
 #define ADC_V_L1				ADC_Value[ADC_IND_SENS1]
@@ -280,7 +243,7 @@
 #define MCCONF_L_MIN_VOLTAGE			12.0		// Minimum input voltage
 #endif
 #ifndef MCCONF_L_MAX_VOLTAGE
-#define MCCONF_L_MAX_VOLTAGE			94.0	// Maximum input voltage
+#define MCCONF_L_MAX_VOLTAGE			72.0	// Maximum input voltage
 #endif
 #ifndef MCCONF_DEFAULT_MOTOR_TYPE
 #define MCCONF_DEFAULT_MOTOR_TYPE		MOTOR_TYPE_FOC
@@ -310,18 +273,16 @@
 #define HW_LIM_CURRENT_IN		-500.0, 500.0
 #define HW_LIM_CURRENT_ABS		0.0, 720.0
 #else
-#define HW_LIM_CURRENT			-800.0, 800.0
-#define HW_LIM_CURRENT_IN		-800.0, 800.0
-#define HW_LIM_CURRENT_ABS		0.0, 1200.0
+#define HW_LIM_CURRENT			-400.0, 400.0
+#define HW_LIM_CURRENT_IN		-400.0, 400.0
+#define HW_LIM_CURRENT_ABS		0.0, 480.0
 #endif
-#define HW_LIM_VIN				11.0, 97.0
+#define HW_LIM_VIN				11.0, 72.0
 #define HW_LIM_ERPM				-200e3, 200e3
 #define HW_LIM_DUTY_MIN			0.0, 0.1
 #define HW_LIM_DUTY_MAX			0.0, 0.99
 #define HW_LIM_TEMP_FET			-40.0, 110.0
 
-// HW-specific functions
-bool hw_sample_shutdown_button(void);
-float hw100_400_get_temp(void);
+float hwks75600_get_temp(void);
 
 #endif /* HW_KS75600_CORE_H_ */
